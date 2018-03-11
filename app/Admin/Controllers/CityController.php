beeb9c1be48f9e79bfa05cb86fee3e0fe7f7f27f
Chris Usick <christopher.usick@gmail.com>
2018-03-11 10:57:59 -0500

    added admin resources

---
+++ app/Admin/Controllers/CityController.php
@@ -0,0 +1,102 @@
+<?php
+
+namespace App\Admin\Controllers;
+
+use App\City;
+
+use Encore\Admin\Form;
+use Encore\Admin\Grid;
+use Encore\Admin\Facades\Admin;
+use Encore\Admin\Layout\Content;
+use App\Http\Controllers\Controller;
+use Encore\Admin\Controllers\ModelForm;
+
+class CityController extends Controller
+{
+    use ModelForm;
+
+    /**
+     * Index interface.
+     *
+     * @return Content
+     */
+    public function index()
+    {
+        return Admin::content(function (Content $content) {
+
+            $content->header('header');
+            $content->description('description');
+
+            $content->body($this->grid());
+        });
+    }
+
+    /**
+     * Edit interface.
+     *
+     * @param $id
+     * @return Content
+     */
+    public function edit($id)
+    {
+        return Admin::content(function (Content $content) use ($id) {
+
+            $content->header('header');
+            $content->description('description');
+
+            $content->body($this->form()->edit($id));
+        });
+    }
+
+    /**
+     * Create interface.
+     *
+     * @return Content
+     */
+    public function create()
+    {
+        return Admin::content(function (Content $content) {
+
+            $content->header('header');
+            $content->description('description');
+
+            $content->body($this->form());
+        });
+    }
+
+    /**
+     * Make a grid builder.
+     *
+     * @return Grid
+     */
+    protected function grid()
+    {
+        return Admin::grid(City::class, function (Grid $grid) {
+
+            $grid->id('ID')->sortable();
+            $grid->column('name');
+            $grid->column('slug');
+            $grid->column('description');
+            $grid->created_at();
+            $grid->updated_at();
+        });
+    }
+
+    /**
+     * Make a form builder.
+     *
+     * @return Form
+     */
+    protected function form()
+    {
+        return Admin::form(City::class, function (Form $form) {
+
+            $form->display('id', 'ID');
+            $form->display('name', 'Name');
+            $form->display('slug', 'Slug');
+            $form->display('description', 'Description');
+            $form->display('created_at', 'Created At');
+            $form->display('updated_at', 'Updated At');
+        });
+    }
+}
